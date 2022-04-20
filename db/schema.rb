eba9db9f1cf28f4cfe1c706a5c761b54a5de5574
laure reina <laure.reina@gmail.com>
2022-04-20 09:59:34 +0200

    add fb et spotify pour devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_131825) do
+ActiveRecord::Schema.define(version: 2022_04_20_073910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,11 @@ ActiveRecord::Schema.define(version: 2022_01_26_131825) do
     t.text "adress"
     t.date "birth_date"
     t.string "username"
+    t.string "provider"
+    t.string "uid"
+    t.string "picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ test/controllers/omiauth_callbacks_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OmiauthCallbacksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
