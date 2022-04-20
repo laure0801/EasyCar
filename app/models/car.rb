ddbb1d960e6ccf739467235f90989e74dee6ffd4
laure reina <laure.reina@gmail.com>
2022-04-20 10:25:40 +0200

    flatpickr

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,10 @@
 class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
